e08f488ec588c24a5ccd6a51d865f36331003b12
jacques FRANC <jacquesn7@gmail.com>
2025-04-15 09:57:38 +0200

    some pt

--- pt.py
+++ pt.py
@@ -6,35 +6,52 @@ import numpy as np
 import vtkmodules.numpy_interface.dataset_adapter as dsa
 import matplotlib.pyplot as plt
 import argparse
+from scipy.optimize import root_scalar
 
-def formula(f : float, a: float = 2.*np.pi, off: float = 0.):
 
-    return np.arccos(((4*np.pi - f - off )/a-1)) * 180/np.pi
+def formula(f: float, a: float = 2. * np.pi, euler : float = 2., off: float = 0.):
+    return np.arccos(((2 * euler * np.pi - f - off) / a - 1)) * 180 / np.pi  # Blunt erroneous
+
+
+def formula_2(f: float, a: float = 2. * np.pi, euler : float = 0., off: float = 0.):
+    return np.arccos( ( 2 * euler * np.pi - f) / 2 / np.pi) * 180 / np.pi  # Real formula
+
+
+def formula_3(f: float, gamma: float, a: float = 2. * np.pi, off: float = 0.):
+    def func(x: float):
+        return (gamma * (1 - np.cos(x)) + f + x - 2*np.pi)  # Real formula
+
+    print(f"values (pi/6,pi/3,pi/2,pi) :")
+    for v in [np.pi / 6, np.pi / 3, np.pi / 2, np.pi]:
+        print(f"{func(v)}", end=",")
+
+    return root_scalar(func, method='bisect', bracket=[0, np.pi]).root * 180. / np.pi  #
+
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser( prog='Post-treat ccpm',
-            description='project phase contact from vertices to cell in VTK format')
-    parser.add_argument('filename',nargs='+')
-    parser.add_argument('-s','--stat',action='store_true')
-    parser.add_argument('-n','--ndisc',nargs=1, type=int)
+    parser = argparse.ArgumentParser(prog='Post-treat ccpm',
+                                     description='project phase contact from vertices to cell in VTK format')
+    parser.add_argument('filename', nargs='+')
+    parser.add_argument('-s', '--stat', action='store_true')
+    parser.add_argument('-n', '--ndisc', nargs=1, type=int)
     args = parser.parse_args()
 
     if args.stat:
         # 510 : tag for S+L+G, 425 for L+G, 340 for S+L and 255 for S+G
-        fig,axs = plt.subplots(len(args.filename)+1,2)
-        vsum = np.ndarray((0,6))
-        for i,file in enumerate(args.filename):
+        fig, axs = plt.subplots(len(args.filename) + 1, 2)
+        vsum = np.ndarray((0, 6))
+        for i, file in enumerate(args.filename):
 
-            tag = None 
+            tag = None
             # t = np.loadtxt(file,skiprows=1,delimiter=',')
-            v = np.loadtxt(file,skiprows=1,delimiter=',')
+            v = np.loadtxt(file, skiprows=1, delimiter=',')
             # u = np.loadtxt(file,skiprows=1,delimiter=',')
             if tag is not None:
-                v510 = v[v[:,5]==tag,:]
+                v510 = v[v[:, 5] == tag, :]
             else:
                 v510 = v
-            v510 = v510[v510[:,4]!=0,:]
-            v510 = v510[v510[:,3]!=0,:]
+            v510 = v510[v510[:, 4] != 0, :]
+            v510 = v510[v510[:, 3] != 0, :]
             vsum = np.concatenate((vsum, v510), axis=0)
             # t510 = t#[t[:,5]==tag,:]
             # t510 = t510[t510[:,4]!=0,:]
@@ -43,54 +60,55 @@ if __name__ == "__main__":
             # u510 = u510[u510[:,4]!=0,:]
             # u510 = u510[u510[:,3]!=0,:]
 
-            xh,yh,h = axs[i,0].hist(v510[:,3],bins=100,label=file)
-            axs[i,0].legend()
-            axs[i,0].set_xlim([-0.5,1.])
+            xh, yh, h = axs[i, 0].hist(v510[:, 3], bins=100, label=file)
+            axs[i, 0].legend()
+            axs[i, 0].set_xlim([-0.5, 1.])
             # xf,yf,f = axs[1,0].hist(t510[:,3],bins=yh,label=args.filename[0])
             # axs[1,0].legend()
             # xg,yg,g = axs[2,0].hist(u510[:,3],bins=yh,label=args.filename[2])
             # axs[2,0].legend()
 
-            xh,yh,h = axs[i,1].hist(v510[:,4],bins=100)
+            xh, yh, h = axs[i, 1].hist(v510[:, 4], bins=100)
             # xf,yf,f = axs[1,1].hist(t510[:,4],bins=yh)
             # xg,yg,g = axs[2,1].hist(u510[:,4],bins=yh)
-            axs[i,1].set_xlim([-0.5,1.])
-        xh,yh,h = axs[-1,0].hist(vsum[:,3],bins=100,label='sum',color='r')
-        axs[-1,0].legend()
-        axs[-1,0].set_xlim([-0.5,1.])
-        xh,yh,h = axs[-1,1].hist(vsum[:,4],bins=100,label='sum',color='r')
-        axs[-1,1].legend()
-        axs[-1,1].set_xlim([-0.5,1.])
+            axs[i, 1].set_xlim([-0.5, 1.])
+        xh, yh, h = axs[-1, 0].hist(vsum[:, 3], bins=100, label='sum', color='r')
+        axs[-1, 0].legend()
+        axs[-1, 0].set_xlim([-0.5, 1.])
+        xh, yh, h = axs[-1, 1].hist(vsum[:, 4], bins=100, label='sum', color='r')
+        axs[-1, 1].legend()
+        axs[-1, 1].set_xlim([-0.5, 1.])
 
         plt.show()
 
     else:
         n = args.ndisc[0]
         r = pv.read(args.filename[0])
-        t = np.loadtxt(args.filename[0].split('.')[0]+'.csv',delimiter=',',skiprows=1)
-        #sorting csv and stl points to match
-        ixc = t[:,0].argsort()
+        t = np.loadtxt(args.filename[0].split('.')[0] + '.csv', delimiter=',', skiprows=1)
+        euler = t[0,-1]
+        # sorting csv and stl points to match
+        ixc = t[:, 0].argsort()
         pts = dsa.numpy_support.vtk_to_numpy(r.GetPoints().GetData())
-        ixm = pts[:,0].argsort()
+        ixm = pts[:, 0].argsort()
         u = np.ndarray(t.shape)
-        for i,ix in enumerate(ixm):
-            u[ix,:] = t[ixc[i],:]
+        for i, ix in enumerate(ixm):
+            u[ix, :] = t[ixc[i], :]
 
         grid = r.cast_to_unstructured_grid()
         tag = dsa.numpyTovtkDataArray(u[:, -2])
         tag.SetName("boundary")
         grid.GetPointData().AddArray(tag)
         #
-        meanC = dsa.numpyTovtkDataArray( .5*(u[:, -5] * u[:, -6]) )
+        meanC = dsa.numpyTovtkDataArray(.5 * (u[:, -5] * u[:, -6]))
         meanC.SetName("meanCurvature")
         grid.GetPointData().AddArray(meanC)
-        gaussianC = dsa.numpyTovtkDataArray( u[:, -5] * u[:, -6] )
+        gaussianC = dsa.numpyTovtkDataArray(u[:, -5] * u[:, -6])
         gaussianC.SetName("gaussianCurvature")
         grid.GetPointData().AddArray(gaussianC)
-        mC = dsa.numpyTovtkDataArray( u[:, -4] )
+        mC = dsa.numpyTovtkDataArray(u[:, -4])
         mC.SetName("mc")
         grid.GetPointData().AddArray(mC)
-        gC = dsa.numpyTovtkDataArray( u[:, -3] )
+        gC = dsa.numpyTovtkDataArray(u[:, -3])
         gC.SetName("gc")
         grid.GetPointData().AddArray(gC)
         #
@@ -99,30 +117,34 @@ if __name__ == "__main__":
         p2c.Update()
         w = vtk.vtkXMLUnstructuredGridWriter()
         w.SetInputData(p2c.GetOutput())
-        w.SetFileName(args.filename[0].split('.')[0]+'.vtu')
+        w.SetFileName(args.filename[0].split('.')[0] + '.vtu')
         w.Update()
         w.Write()
-        #w = vtk.vtkXMLPolyDataWriter()
+        w.SetInputData(grid)
+        w.SetFileName(args.filename[0].split('.')[0] + '_pt.vtu')
+        w.Update()
+        w.Write()
+        # w = vtk.vtkXMLPolyDataWriter()
 
         qual = vtk.vtkMeshQuality()
         qual.SetInputData(p2c.GetOutput())
         qual.SetTriangleQualityMeasureToArea()
         qual.Update()
 
-        tag = 5.1
-        #selection work
+        tag = 510
+        # selection work
         selectionNode = vtk.vtkSelectionNode()
         selectionNode.SetFieldType(vtk.vtkSelectionNode.CELL)
         selectionNode.SetContentType(vtk.vtkSelectionNode.INDICES)
         ids = vtk.vtkIdTypeArray()
         for icell in range(qual.GetOutput().GetNumberOfCells()):
-            if(qual.GetOutput().GetCellData().GetArray('boundary').GetValue(icell)>=tag):
+            if (qual.GetOutput().GetCellData().GetArray('boundary').GetValue(icell) >= tag):
                 ids.InsertNextValue(icell)
         selectionNode.SetSelectionList(ids)
         selection = vtk.vtkSelection()
         selection.AddNode(selectionNode)
         extract = vtk.vtkExtractSelection()
-        extract.SetInputConnection(0,qual.GetOutputPort())
+        extract.SetInputConnection(0, qual.GetOutputPort())
         extract.SetInputData(1, selection)
         extract.Update()
         print(f"there is {extract.GetOutput().GetNumberOfCells()} cells in the selection")
@@ -139,21 +161,27 @@ if __name__ == "__main__":
         region = dsa.numpy_support.vtk_to_numpy(conn.GetOutput().GetCellData().GetArray('RegionId'))
         area = dsa.numpy_support.vtk_to_numpy(conn.GetOutput().GetCellData().GetArray(5))
 
-        print(f"global angle : {4*np.pi} : { np.sum(garea*dsa.numpy_support.vtk_to_numpy(p2c.GetOutput().GetCellData().GetArray('gc'))) } : "
-              f"{ np.sum(garea*( dsa.numpy_support.vtk_to_numpy(p2c.GetOutput().GetCellData().GetArray('gaussianCurvature')) + 0.*dsa.numpy_support.vtk_to_numpy(p2c.GetOutput().GetCellData().GetArray('meanCurvature'))))}")
+        print(
+            f"global angle : {2 * euler * np.pi} : {np.sum(garea * dsa.numpy_support.vtk_to_numpy(p2c.GetOutput().GetCellData().GetArray('gc')))} : "
+            f"{np.sum(garea * (dsa.numpy_support.vtk_to_numpy(p2c.GetOutput().GetCellData().GetArray('gaussianCurvature')) + 0. * dsa.numpy_support.vtk_to_numpy(p2c.GetOutput().GetCellData().GetArray('meanCurvature'))))}")
+        print(f"number of faces {p2c.GetOutput().GetNumberOfCells()}")
 
         number_of_valid = 0
-        s,e = conn.GetOutput().GetCellData().GetArray('RegionId').GetRange()
-        for i in np.arange(s,e+1):
+        s, e = conn.GetOutput().GetCellData().GetArray('RegionId').GetRange()
+        for i in np.arange(s, e + 1):
 
-            #eliminate minor contacts
-            if(region[region==i].shape[0]>15):
+            # eliminate minor contacts
+            if (region[region == i].shape[0] > 25):
                 number_of_valid += 1
 
-                print(f"{i} : number of elements {np.sum(region==i)} for a global area of {np.sum(area[region==i])}")
-                print(f"{i} : ring value : { (f1:=np.sum(area[region == i]*dsa.numpy_support.vtk_to_numpy(conn.GetOutput().GetCellData().GetArray('gc'))[region == i])) } : "
-                      f"{ (f2:=np.sum(area[region==i]*( dsa.numpy_support.vtk_to_numpy(conn.GetOutput().GetCellData().GetArray('gaussianCurvature'))[region == i] + dsa.numpy_support.vtk_to_numpy(conn.GetOutput().GetCellData().GetArray('meanCurvature'))[region == i]))) }")
-                print(f"{i} : formula : {formula(f1)} : {formula(f2)}")
+                print(
+                    f"{i} : number of elements {np.sum(region == i)} for a global area of {np.sum(area[region == i])}")
+                print(
+                    f"{i} : ring value : {(f1 := np.sum(area[region == i] * dsa.numpy_support.vtk_to_numpy(conn.GetOutput().GetCellData().GetArray('gc'))[region == i]))} : "
+                    f"{(f2 := np.sum(area[region == i] * (dsa.numpy_support.vtk_to_numpy(conn.GetOutput().GetCellData().GetArray('gaussianCurvature'))[region == i] + dsa.numpy_support.vtk_to_numpy(conn.GetOutput().GetCellData().GetArray('meanCurvature'))[region == i])))}")
+                print(f"{i} : formula : {formula(f1,euler)} : {formula(f2,euler)}")
+                print(f"{i} : formula_2 : {formula_2(f1,euler)} : {formula_2(f2,euler)}")
+                # print(f"\n {i} : formula_3 : {formula_3(f1*.9, .9*2*np.pi)} ")
 
                 # print(f"formula from geometry")
                 # cc = (vtk.vtkCellCenters())
@@ -175,8 +203,9 @@ if __name__ == "__main__":
                 # print(f"ring value (p={p}) : {f1p} : {f2p} ")
                 # print(f"formula (p={p}) ): {formula(f1p, np.pi, +3/2*np.pi)} : {formula(f2p,np.pi,0)}")
 
-                #np.arccos(((4*np.pi - 0.88 )/2/np.pi-1)) * 180/np.pi        p
+                # np.arccos(((4*np.pi - 0.88 )/2/np.pi-1)) * 180/np.pi        p
 
-        print(f"A : ring value : { (f1:=np.sum(area*dsa.numpy_support.vtk_to_numpy(conn.GetOutput().GetCellData().GetArray('gc')))/number_of_valid) } : "
-        f"{ (f2:=np.sum(area*( dsa.numpy_support.vtk_to_numpy(conn.GetOutput().GetCellData().GetArray('gaussianCurvature')) + dsa.numpy_support.vtk_to_numpy(conn.GetOutput().GetCellData().GetArray('meanCurvature'))))/number_of_valid) }")
+        print(
+            f"A : ring value : {(f1 := np.sum(area * dsa.numpy_support.vtk_to_numpy(conn.GetOutput().GetCellData().GetArray('gc'))) / number_of_valid)} : "
+            f"{(f2 := np.sum(area * (dsa.numpy_support.vtk_to_numpy(conn.GetOutput().GetCellData().GetArray('gaussianCurvature')) + dsa.numpy_support.vtk_to_numpy(conn.GetOutput().GetCellData().GetArray('meanCurvature')))) / number_of_valid)}")
         print(f"A : formula : {formula(f1)} : {formula(f2)}")
