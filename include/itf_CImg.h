63c7cc57ce581619f57d0b789ce580d994271212
jacques FRANC <jacquesn7@gmail.com>
2025-07-22 10:57:49 +0200

    licencing

--- include/itf_CImg.h
+++ include/itf_CImg.h
@@ -1,10 +1,24 @@
 /*
- * itf_CImg.h
+ * CCPM (Curvature Capture in Porous Media)
+ * Copyright (C) 2025 by jacquesn7 (jacquesn7@gmail.com)
  *
- *  Created on: 20 déc. 2018
- *      Author: jfranc
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ *
+ * created by: jacquesn7 (jacquesn7@gmail.com)
+ *
  */
-
+#ifnde
 #ifndef ITF_CIMG_H_
 #define ITF_CIMG_H_
 
